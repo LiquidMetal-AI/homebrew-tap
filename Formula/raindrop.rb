300019474ae3a468a9c10cdca7a73ab7ba559e2c
Ian Schenck <ian@liquidmetal.ai>
2025-10-27 20:31:56 -0600

    Add raindrop v0.9.1 formula (#3)

---
+++ Formula/raindrop.rb
@@ -0,0 +1,17 @@
+class Raindrop < Formula
+  desc "Raindrop CLI, By LiquidMetal.AI"
+  homepage "https://www.npmjs.com/package/@liquidmetal-ai/raindrop"
+  url "https://registry.npmjs.org/@liquidmetal-ai/raindrop/-/raindrop-0.9.1.tgz"
+  sha256 "6db4a8f27fd9d840fded332fb22eca3bf24cbb946a61f24d6baed3ff44f16046"
+  
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "#{bin}/raindrop", "--version"
+  end
+end
