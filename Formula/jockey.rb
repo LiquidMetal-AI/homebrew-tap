0db31b0f961a3a4332f5ecfd85f5361ddf730862
Ian Schenck <ian@liquidmetal.ai>
2025-10-29 18:04:55 -0600

    Update jockey formula to v0.0.1-alpha5 (#7)

--- Formula/jockey.rb
+++ Formula/jockey.rb
@@ -1,9 +1,9 @@
 class Jockey < Formula
   desc "AI coding assistant with powerful code intelligence tools"
   homepage "https://github.com/LiquidMetal-AI/homebrew-tap"
-  version "0.0.1-alpha4"
+  version "0.0.1-alpha5"
   url "https://github.com/LiquidMetal-AI/homebrew-tap/releases/download/jockey-v#{version}/jockey-v#{version}-darwin-universal.tar.gz"
-  sha256 "6b3e91339f8f461f72ac7aae7fe7deaafbd4fae0b101cfc288df4f970e4ab7e1"
+  sha256 "bfdfebf0f8365dba0a597209042ddbe28a9df9988828db18a0af21f9fc0cfe6f"
 
   def install
     bin.install "jockey"
