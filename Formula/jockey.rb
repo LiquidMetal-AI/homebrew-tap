11922bd05773b2fa808c3acace37ad6fdd5e9728
Ian Schenck <ian@liquidmetal.ai>
2025-10-31 08:57:13 -0600

    Update jockey to v0.0.1-alpha8 (#10)
    * Update jockey to v0.0.1-alpha8

--- Formula/jockey.rb
+++ Formula/jockey.rb
@@ -1,8 +1,8 @@
 class Jockey < Formula
   desc "AI coding assistant with powerful code intelligence tools"
   homepage "https://github.com/LiquidMetal-AI/homebrew-tap"
-  url "https://github.com/LiquidMetal-AI/homebrew-tap/releases/download/jockey-v0.0.1-alpha7/jockey-v0.0.1-alpha7-darwin-universal.tar.gz"
-  sha256 "f79b31d285110f58a1583584ce173546d35eaeed17609a2d9ce17b0d7af18603"
+  url "https://github.com/LiquidMetal-AI/homebrew-tap/releases/download/jockey-v0.0.1-alpha8/jockey-v0.0.1-alpha8-darwin-universal.tar.gz"
+  sha256 "0c86bb4a18f1c94b07c74ce39fc3c9dd17232415a2e3e126b549abfb632eb065"
 
   def install
     bin.install "jockey"
